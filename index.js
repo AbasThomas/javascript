32ba40750897ec254eaae1f33aee24f469afa55d
CodeLord7x <abasienyenethomas2@gmail.com>
2024-11-13 15:24:51 -0800

    Add index.js and readme.md

---
+++ index.js
@@ -0,0 +1,54 @@
+// // var ice = "Michael"
+// // console.log(ice)
+
+// // console.log(typeof(ice))
+
+// var fisrtnum = 1
+// var secnum = 2
+// var result = fisrtnum - secnum
+// console.log(result)
+
+// var fisrtnum = 1
+// var secnum = 2
+// var result = fisrtnum * secnum
+// console.log(result)
+
+
+// var fisrtnum = 1
+// var secnum = 2
+// var result = fisrtnum + secnum
+// console.log(result)
+
+// var fisrtnum = 1
+// var secnum = 2
+// var result = fisrtnum**secnum
+// console.log(result)
+
+
+// 1. Calculate the area of a circle => Area = π x radius²
+var pi = 22/7
+var radius= 3
+var area = pi * radius**2
+console.log(area)
+
+// 2. Calculate the area of a triangle => Area = ½ x base x height
+var base = 4
+var height = 3
+var area = 1/2 * base * height
+console.log(area)
+
+// 3. Calculate the simple interest => Simple Interest = Principal x Rate x Time / 100
+var Principal = 2
+var Rate = 2
+var Time = 2
+var Interest = Principal * Rate * Time/100
+console.log(Interest)
+
+// Calculate the quadratic formula => x = -b ± ✓b² - 4ac / 2a
+var b = 1
+var a = 2
+var c = 3
+var x = -b + Math.sqrt(b) ** 2 - 4 * a * c /2 *a
+var x = -b - Math.sqrt(b) ** 2 - 4 * a * c /2 *a
+
+console.log(x)
